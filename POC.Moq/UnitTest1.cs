497fdc724c02a01ac13171572bb9f60fdc250389
vijay33tube <kulavade.vijay@gmail.com>
2015-06-10 10:39:49 -0500

    New project added

---
+++ POC.Moq/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace POC.Moq
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var moqEmailService = new Mock<IEmailService>();
+
+            moqEmailService.Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            var emailer = new Emailer(moqEmailService.Object);
+
+            emailer.SendBatchEmails();
+        }
+    }
+
+    public class Emailer
+    {
+        private IEmailService emailService = null;
+        public Emailer(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+        public void SendBatchEmails()
+        {
+            if(!emailService.SendMail("a","b"))
+            {
+                throw new Exception("Some error");
+            }
+        }
+    }
+
+    public interface IEmailService
+    {
+        bool SendMail(string email, string message);
+    }
+
+    public class EmailService : IEmailService
+    { 
+        public bool SendMail(string email, string message)
+        {
+            return false;
+        }
+    }
+}
